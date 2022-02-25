35b8a42cedb0da8bb944c0e423455c33049a8aca
m.saccoccini1997@gmail.com <m.saccoccini1997@gmail.com>
2022-02-25 18:29:23 +0100

    soluzione vue-hello

---
+++ app.js
@@ -0,0 +1,12 @@
+const app = new Vue({
+
+    el :'#app',
+    data:{
+        message :'Imagination means nothing,without doing.',
+        imgUrl :'https://picsum.photos/200/300',
+    }
+
+
+
+    
+});
